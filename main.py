b6afb91b3ca87f78208a0680355a0170d68a467e
Erekle Shindagoridze <130274791+Ereklefullstack@users.noreply.github.com>
2024-07-24 09:11:59 +0400

    Save Data in CSV file

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import sqlite3
 import pandas as pd
+import os
 
 # Specify the path of your SQLite database
-database_path = 'path_to_your_database.db'
+database_path = os.path.abspath('path_to_your_database.db')
 
 # Connect to the SQLite database
-conn = sqlite3.connect(database_path)
+try:
+    conn = sqlite3.connect(database_path)
+except sqlite3.OperationalError as e:
+    print(f"Error: {e}")
+    exit()
 
 # Retrieve the list of tables
 query = "SELECT name FROM sqlite_master WHERE type='table';"
@@ -15,13 +20,13 @@ tables_df = pd.read_sql_query(query, conn)
 print("Tables in the database:")
 print(tables_df)
 
-# If you want to display a specific table
-if not tables_df.empty:
-    table_name = tables_df['name'].iloc[0]  # Gets the name of the first table
+# Save the information of all tables to CSV files
+for table_name in tables_df['name']:
     data_query = f'SELECT * FROM {table_name}'
     df = pd.read_sql_query(data_query, conn)
-    print(f"\nData from the table '{table_name}':")
-    print(df)
+    csv_file_path = f'{table_name}.csv'
+    df.to_csv(csv_file_path, index=False)
+    print(f"Data from the table '{table_name}' has been written to '{csv_file_path}'")
 
 # Close the connection to the database
 conn.close()
